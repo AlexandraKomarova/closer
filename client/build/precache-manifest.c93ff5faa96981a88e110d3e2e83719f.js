af626a7d7d6497f614eb18617bbc8a8b11cd819b
Sonechka M <s212288s@gmail.com>
2020-04-05 11:56:03 -0400

    dep

---
+++ client/build/precache-manifest.c93ff5faa96981a88e110d3e2e83719f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12b1fb16cb449bfed9b4ed6da93d1d5a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02d5162fe7125958111c",
+    "url": "/static/css/main.28ec31e2.chunk.css"
+  },
+  {
+    "revision": "7e827e5381d2c543f8b7",
+    "url": "/static/js/2.d5468aa4.chunk.js"
+  },
+  {
+    "revision": "603816eec26c1aff4a38bef8d08d40a6",
+    "url": "/static/js/2.d5468aa4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02d5162fe7125958111c",
+    "url": "/static/js/main.b5c54995.chunk.js"
+  },
+  {
+    "revision": "65762bcbee126357b070",
+    "url": "/static/js/runtime-main.35eb2b37.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
