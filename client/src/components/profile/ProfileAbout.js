f3a385b2ed2d9a811e7527afe15fd54430d28055
Sonechka M <s212288s@gmail.com>
2020-04-09 18:25:04 -0400

    styles

--- client/src/components/profile/ProfileAbout.js
+++ client/src/components/profile/ProfileAbout.js
@@ -11,12 +11,12 @@ const ProfileAbout = ({
   <div className='profile-aboutp-2'>
     {bio && (
       <Fragment>
-        <h2 className=''>{name.trim().split(' ')[0]}'s Bio</h2>
+        {/* <h2 className=''>{name.trim().split(' ')[0]}'s Bio</h2> */}
         <p>{bio}</p>
         <div className='line' />
       </Fragment>
     )}
-    <h2 className=''>Skill Set</h2>
+    <h2 className=''>I have experience with:</h2>
     <div className='skills'>
       {skills.map((skill, index) => (
         <div key={index} className='p-1'>
